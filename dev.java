d52829e72a1e31fbc6c7e28f2dc03dd528e95c29
mrkns420 <mollirevanth123@gmail.com>
2023-11-10 15:35:28 +0530

    my code files

---
+++ dev.java
@@ -0,0 +1,13 @@
+class A
+{
+public static void main(String args[]){
+
+System.out.println("hii we are developers");
+
+
+
+}
+
+
+
+}
---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a=10,b=20;
+c=a+b;
+printf("sum of a,b",%d,c);
+
+
+}
